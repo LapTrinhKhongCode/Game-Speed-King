be5f5ad61a2e62d9858c3442eb11d80a2652ba63
Do Van Duc Anh <dovanducanh06@gmail.com>
2024-06-19 21:31:28 +0700

    Add project files.

---
+++ Version 1/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Version_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.XePlayer = new System.Windows.Forms.PictureBox();
+            this.XeComputer = new System.Windows.Forms.PictureBox();
+            this.timerDiChuyenXe = new System.Windows.Forms.Timer(this.components);
+            this.NutPlay = new System.Windows.Forms.PictureBox();
+            this.VuNo = new System.Windows.Forms.PictureBox();
+            this.lblDiemSo = new System.Windows.Forms.Label();
+            this.VienDan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.XePlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XeComputer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NutPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VuNo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VienDan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // XePlayer
+            // 
+            this.XePlayer.Image = ((System.Drawing.Image)(resources.GetObject("XePlayer.Image")));
+            this.XePlayer.Location = new System.Drawing.Point(573, 331);
+            this.XePlayer.Name = "XePlayer";
+            this.XePlayer.Size = new System.Drawing.Size(78, 149);
+            this.XePlayer.TabIndex = 0;
+            this.XePlayer.TabStop = false;
+            this.XePlayer.Click += new System.EventHandler(this.XePlayer_Click);
+            this.XePlayer.MouseDown += new System.Windows.Forms.MouseEventHandler(this.XePlayer_MouseDown);
+            this.XePlayer.MouseMove += new System.Windows.Forms.MouseEventHandler(this.XePlayer_MouseMove);
+            this.XePlayer.MouseUp += new System.Windows.Forms.MouseEventHandler(this.XePlayer_MouseUp);
+            // 
+            // XeComputer
+            // 
+            this.XeComputer.Image = ((System.Drawing.Image)(resources.GetObject("XeComputer.Image")));
+            this.XeComputer.Location = new System.Drawing.Point(85, 112);
+            this.XeComputer.Name = "XeComputer";
+            this.XeComputer.Size = new System.Drawing.Size(76, 149);
+            this.XeComputer.TabIndex = 1;
+            this.XeComputer.TabStop = false;
+            this.XeComputer.Click += new System.EventHandler(this.XeComputer_Click);
+            // 
+            // timerDiChuyenXe
+            // 
+            this.timerDiChuyenXe.Interval = 1;
+            this.timerDiChuyenXe.Tick += new System.EventHandler(this.timerDiChuyenXe_Tick);
+            // 
+            // NutPlay
+            // 
+            this.NutPlay.Image = ((System.Drawing.Image)(resources.GetObject("NutPlay.Image")));
+            this.NutPlay.Location = new System.Drawing.Point(632, 33);
+            this.NutPlay.Name = "NutPlay";
+            this.NutPlay.Size = new System.Drawing.Size(56, 52);
+            this.NutPlay.TabIndex = 2;
+            this.NutPlay.TabStop = false;
+            this.NutPlay.Click += new System.EventHandler(this.NutPlay_Click);
+            // 
+            // VuNo
+            // 
+            this.VuNo.Image = ((System.Drawing.Image)(resources.GetObject("VuNo.Image")));
+            this.VuNo.Location = new System.Drawing.Point(215, 341);
+            this.VuNo.Name = "VuNo";
+            this.VuNo.Size = new System.Drawing.Size(154, 163);
+            this.VuNo.TabIndex = 3;
+            this.VuNo.TabStop = false;
+            // 
+            // lblDiemSo
+            // 
+            this.lblDiemSo.AutoSize = true;
+            this.lblDiemSo.Font = new System.Drawing.Font("Times New Roman", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDiemSo.Location = new System.Drawing.Point(12, 19);
+            this.lblDiemSo.Name = "lblDiemSo";
+            this.lblDiemSo.Size = new System.Drawing.Size(107, 37);
+            this.lblDiemSo.TabIndex = 4;
+            this.lblDiemSo.Text = "Score:";
+            // 
+            // VienDan
+            // 
+            this.VienDan.Image = ((System.Drawing.Image)(resources.GetObject("VienDan.Image")));
+            this.VienDan.Location = new System.Drawing.Point(429, 112);
+            this.VienDan.Name = "VienDan";
+            this.VienDan.Size = new System.Drawing.Size(30, 100);
+            this.VienDan.TabIndex = 5;
+            this.VienDan.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(748, 741);
+            this.Controls.Add(this.VienDan);
+            this.Controls.Add(this.lblDiemSo);
+            this.Controls.Add(this.VuNo);
+            this.Controls.Add(this.NutPlay);
+            this.Controls.Add(this.XeComputer);
+            this.Controls.Add(this.XePlayer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Speed King";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.XePlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.XeComputer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.NutPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VuNo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.VienDan)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox XePlayer;
+        private System.Windows.Forms.PictureBox XeComputer;
+        private System.Windows.Forms.Timer timerDiChuyenXe;
+        private System.Windows.Forms.PictureBox NutPlay;
+        private System.Windows.Forms.PictureBox VuNo;
+        private System.Windows.Forms.Label lblDiemSo;
+        private System.Windows.Forms.PictureBox VienDan;
+    }
+}
+
---
+++ Version 1/Form1.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Version_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int x_player, y_player;
+        int x_computer, y_computer;
+        int x_viendan, y_viendan;
+        int DiemSo = 0;
+
+        PictureBox[] arrXe = new PictureBox[2];
+        PictureBox[] arrDan = new PictureBox[5];
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+            // thay đổi kích thước Form.
+            this.Size = new Size(766, 700);
+            XePlayer.Size = new Size(75, 150);
+            XeComputer.Size = new Size(75, 150);
+
+            XePlayer.Location = new Point(300, 400);
+            NutPlay.Size = new Size(50, 50);
+            NutPlay.Location = new Point(this.Width - NutPlay.Width - 10, 10);
+
+            VuNo.Size = new Size(150, 150);
+            VuNo.Visible = false;
+
+            VienDan.Size = new Size(30, 100);
+            VienDan.Visible = false;
+
+
+            // tọa độ ban đầu của xe Player
+            x_player = XePlayer.Location.X;
+            y_player = XePlayer.Location.Y;
+
+            // Tọa độ ban đầu của xe Computer.
+            y_computer = 0;
+            x_computer = 150;
+
+            timerDiChuyenXe.Interval = 10; // cứ 1/10 giây sẽ gọi hàm tick
+            
+        }
+
+        bool KiemTraNhan = false; // cờ hiệu
+        Point ToaDo;
+
+        private void XePlayer_MouseDown(object sender, MouseEventArgs e)
+        {
+            KiemTraNhan = true; // đã nhấn chuột.
+            ToaDo = e.Location; // cập nhật tọa độ khi nhấn chuột xuống.
+        }
+        
+        private void XePlayer_MouseMove(object sender, MouseEventArgs e)
+        {
+            // phải là đang còn nhấn xuống thì mới cho phép di chuyển.
+            if (KiemTraNhan == true)
+            {
+                PictureBox a = (PictureBox)(sender);
+                Point toadobandau = a.Location; // Lấy tọa độ ban đầu của tấm hình.
+                
+                // e.Location: Là tọa độ mới sau khi đã di chuyển chuột
+                // ToaDo.x: Là tọa độ ngay lúc vừa mới nhấn chuột xuống
+                // => khoảng cách dịch chuyển của tấm hình sẽ là hiệu của lần dịch sau - lúc đầu
+                toadobandau.X += e.Location.X - ToaDo.X;
+                toadobandau.Y += e.Location.Y - ToaDo.Y;
+
+                if (toadobandau.X <= 0)
+                {
+                    toadobandau.X = 0;
+                }
+                if (toadobandau.X >= this.Width - XePlayer.Width - 10)
+                {
+                    toadobandau.X = this.Width - XePlayer.Width - 10;
+                }
+                if (toadobandau.Y <= 0)
+                {
+                    toadobandau.Y = 0;
+                }
+                if (toadobandau.Y >= this.Height - XePlayer.Height - 10)
+                {
+                    toadobandau.Y = this.Height - XePlayer.Height - 10;
+                }
+
+                // cập nhật lại mới tọa độ cho tấm hình.
+                XePlayer.Location = toadobandau; // cập nhật lại vị trí mới sau khi di chuyển.
+
+
+                
+            }
+        }
+
+        private void XePlayer_MouseUp(object sender, MouseEventArgs e)
+        {
+            KiemTraNhan = false;
+        }
+
+        private void timerDiChuyenXe_Tick(object sender, EventArgs e)
+        {
+            
+            y_computer += 10;
+
+            // cập nhật lại tọa độ mới cho xe computer
+            XeComputer.Location = new Point(x_computer, y_computer);
+
+            // Cho xe đi hết Form thì quy trở lại ban đầu.
+            if (y_computer >= this.Height)
+            {
+                DiemSo++; // tăng lên 1 điểm.
+                lblDiemSo.Text = "Score: " + DiemSo.ToString();
+                timerDiChuyenXe.Interval = 10;
+
+                //if (DiemSo == 5)
+                //{
+                //    timerDiChuyenXe.Interval = 50;
+                //}
+                //else if (DiemSo == 10)
+                //{
+                //    timerDiChuyenXe.Interval = 20;
+                //}
+                //else if (DiemSo == 15)
+                //{
+                //    timerDiChuyenXe.Interval = 10;
+                //}
+                //else if (DiemSo > 20)
+                //{
+                //    timerDiChuyenXe.Interval = 1;
+                //}
+
+                y_computer = 0;
+
+                // thay đổi lại x nằm trong đoạn từ 0 --> this.Width - XeComputer.Width
+                Random rd = new Random();
+                x_computer = rd.Next(0, this.Width - XeComputer.Width);
+            }
+
+            // Kiểm tra va chạm giữa 2 xe. (giữa 2 picture box)
+            if (XeComputer.Visible == true && XePlayer.Visible == true)
+            {
+                if (XeComputer.Location.Y + XeComputer.Height >= XePlayer.Location.Y)
+                {
+                    if (XePlayer.Location.X >= XeComputer.Location.X - XePlayer.Width && XePlayer.Location.X <= XeComputer.Location.X + XePlayer.Width)
+                    {
+                        timerDiChuyenXe.Stop(); // dừng lại timer lại.
+                        VuNo.Visible = true; // Hiện vụ nổ.
+                        XePlayer.Visible = false; // mất xe
+                        XeComputer.Visible = false; // mất xe
+
+                        VuNo.Location = new Point(XePlayer.Location.X, XePlayer.Location.Y);
+
+                        DialogResult luachon = MessageBox.Show("Bạn có muốn tiếp tục trò chơi ?", "Thông Báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (luachon == DialogResult.Yes)
+                        {
+                            DiemSo = 0;
+                            lblDiemSo.Text = "Score: ";
+
+                            timerDiChuyenXe.Start(); // choi lai tu dau
+                            KiemTraNhan = false; // reset lại cờ hiệu
+                            VuNo.Visible = false; // ẩn vụ nổ
+                            XeComputer.Visible = true; // hiện lại xe
+                            XePlayer.Visible = true; // hiện lại xe
+
+                            y_computer = 0;
+
+                            // thay đổi lại x nằm trong đoạn từ 0 --> this.Width - XeComputer.Width
+                            Random rd = new Random();
+                            x_computer = rd.Next(0, this.Width - XeComputer.Width);
+
+                            XeComputer.Location = new Point(x_computer, y_computer); // cập nhật lại 
+                        }
+                        else
+                        {
+                            DaChoi = true;
+                            Application.Exit();
+                        }
+
+                    }
+                }
+            }
+            
+
+            // Bắn đạn
+            if (VienDan.Visible == true)
+            {
+                y_viendan -= 10;
+                VienDan.Location = new Point(x_viendan, y_viendan);
+
+                // Kiểm tra sự va chạm giữa viên đạn với xe Computer
+                if (XeComputer.Location.Y + XeComputer.Height >= VienDan.Location.Y)
+                {
+                    if (VienDan.Location.X >= XeComputer.Location.X - VienDan.Width && VienDan.Location.X <= XeComputer.Location.X + XeComputer.Width)
+                    {
+                        XeComputer.Visible = false; // Biến mất
+                        VienDan.Visible = false; // biến mất.
+                    }
+                }
+            }
+
+            // Nếu như y_XeComputer mà chạm đáy thì lại cho xe hiện lại
+            if (y_computer + XeComputer.Height >= this.Height)
+            {
+                y_computer = 0;
+                Random rd = new Random();
+                x_computer = rd.Next(0, this.Width - XeComputer.Width);
+                XeComputer.Location = new Point(x_computer, y_computer); // cập nhật lại 
+
+                XeComputer.Visible = true;
+            }
+
+        }
+
+        bool DaChoi = false;
+        int dem = 0;
+
+        private void XeComputer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void XePlayer_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void NutPlay_Click(object sender, EventArgs e)
+        {
+           
+            dem++;
+            if (dem % 2 != 0)
+            {
+                timerDiChuyenXe.Start();
+            }
+            else
+            {
+                timerDiChuyenXe.Stop();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (DaChoi == true)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                MessageBox.Show("ê ! Phải chơi rồi nếu thua mới tắt được");
+                e.Cancel = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+                VienDan.Visible = true;
+                y_viendan = XePlayer.Location.Y - VienDan.Height - 10;
+                x_viendan = XePlayer.Location.X + XePlayer.Width / 2;
+
+                VienDan.Location = new Point(x_viendan, y_viendan);
+            }
+        }
+    }
+}
